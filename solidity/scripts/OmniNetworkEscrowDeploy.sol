601eca60b09072a60bd65b227cff2cabb7f4c7a5
nicosampler <nf.dominguez.87@gmail.com>
2024-02-06 15:25:26 -0300

    OmniNetworkEscrow: start/stop broadcast

--- solidity/scripts/OmniNetworkEscrowDeploy.sol
+++ solidity/scripts/OmniNetworkEscrowDeploy.sol
@@ -2,20 +2,22 @@
 pragma solidity >=0.8.4 <0.9.0;
 
 // solhint-disable-next-line no-console
-import {console} from "forge-std/console.sol";
-import {OmniNetworkEscrow} from "../contracts/OmniNetworkEscrow.sol";
-import {Script} from "forge-std/Script.sol";
-import {ScriptingLibrary} from "./ScriptingLibrary/ScriptingLibrary.sol";
+import {console} from 'forge-std/console.sol';
+import {OmniNetworkEscrow} from '../contracts/OmniNetworkEscrow.sol';
+import {Script} from 'forge-std/Script.sol';
+import {ScriptingLibrary} from './ScriptingLibrary/ScriptingLibrary.sol';
 
 contract OmniNetworkEscrowDeploy is Script, ScriptingLibrary {
-    function run() public {
-        vm.createSelectFork(vm.rpcUrl(vm.envString("SEPOLIA_RPC")));
+  uint256 public deployer = vm.envUint('DEPLOYER_PRIVATE_KEY');
 
-        console.log(vm.envString("SEPOLIA_RPC"));
+  function run() public {
+    vm.createSelectFork(vm.rpcUrl(vm.envString('ETHEREUM_SEPOLIA_RPC')));
 
-        OmniNetworkEscrow _escrow = new OmniNetworkEscrow();
+    vm.startBroadcast(deployer);
+    OmniNetworkEscrow _escrow = new OmniNetworkEscrow();
+    vm.stopBroadcast();
 
-        // solhint-disable-next-line no-console
-        console.log("OmniNetworkEscrow deployed to:", address(_escrow));
-    }
+    // solhint-disable-next-line no-console
+    console.log('OmniNetworkEscrow deployed to:', address(_escrow));
+  }
 }
