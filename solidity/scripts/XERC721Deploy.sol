62a90b91b7bfed19c725db2f0a3d6cb5681214f1
Paterson <86971279+paterson1@users.noreply.github.com>
2024-02-26 12:25:27 -0300

    deploy scripts improvemnets

--- solidity/scripts/XERC721Deploy.sol
+++ solidity/scripts/XERC721Deploy.sol
@@ -6,9 +6,8 @@ import {console} from 'forge-std/console.sol';
 import {Test} from 'forge-std/Test.sol';
 import {Script} from 'forge-std/Script.sol';
 import {stdJson} from 'forge-std/StdJson.sol';
-import {XERC20} from '../contracts/XERC20.sol';
-import {XERC20Lockbox} from '../contracts/XERC20Lockbox.sol';
-import {XERC20Factory, IXERC20Factory} from '../contracts/XERC20Factory.sol';
+import {XERC721} from '../contracts/XERC721.sol';
+import {XERC721Factory, IXERC721Factory} from '../contracts/XERC721Factory.sol';
 import {ScriptingLibrary} from './ScriptingLibrary/ScriptingLibrary.sol';
 
 // NOTE: IMPORTANT! Struct members should be order by ALPHABETICAL order. DO NOT modify them.
@@ -22,27 +21,27 @@ struct BridgeDetails {
 
 struct ChainDetails {
   BridgeDetails[] bridgeDetails; // The array of bridges to configure for this chain
-  address erc20; // The address of the ERC20 canonical token of that chain (address(0) if none)
-  address governor; // The governor address of the xERC20
-  bool isNativeGasToken; // Whether or not the token is the native gas token of the chain. E.g. Are you deploying an xERC20 for MATIC in Polygon?
+  address erc721; // The address of the ERC721 canonical token of that chain (address(0) if none)
+  address governor; // The governor address of the xERC721
+  bool isNativeGasToken; // Whether or not the token is the native gas token of the chain. E.g. Are you deploying an xERC721 for MATIC in Polygon?
   string rpcEnvName; // The name of the RPC to use from the .env file
 }
 
 struct DeploymentConfig {
   ChainDetails[] chainDetails;
-  string name; // The name to use for the xERC20
-  string symbol; // The symbol to use for the xERC20
+  string name; // The name to use for the xERC721
+  string symbol; // The symbol to use for the xERC721
 }
 
-contract XERC20Deploy is Script, ScriptingLibrary {
+contract XERC721Deploy is Script, ScriptingLibrary {
   using stdJson for string;
 
   uint256 public deployer = vm.envUint('DEPLOYER_PRIVATE_KEY');
-  address public factoryAddress = vm.envAddress('XERC20_FACTORY');
-  XERC20Factory public factory = XERC20Factory(factoryAddress);
+  address public factoryAddress = vm.envAddress('XERC721_FACTORY');
+  XERC721Factory public factory = XERC721Factory(factoryAddress);
 
   function run() public {
-    string memory _json = vm.readFile('./solidity/scripts/xerc20-deployment-config.json');
+    string memory _json = vm.readFile('./solidity/scripts/xerc721-deployment-config.json');
     DeploymentConfig memory _data = abi.decode(_json.parseRaw('.'), (DeploymentConfig));
     uint256 _chainAmount = _data.chainDetails.length;
     address[] memory _tokens = new address[](_chainAmount);
@@ -69,29 +68,19 @@ contract XERC20Deploy is Script, ScriptingLibrary {
         _mintLimits[_bridgeIndex] = _bridgeDetails[_bridgeIndex].mintLimit;
       }
 
-      // deploy xerc20
-      address _xerc20 = factory.deployXERC20(_data.name, _data.symbol, _mintLimits, _burnLimits, _bridges);
+      // deploy xerc721
+      address _xerc721 = factory.deployXERC721(_data.name, _data.symbol, _mintLimits, _burnLimits, _bridges);
 
-      // deploy lockbox if needed
-      address _lockbox;
-      if (_chainDetails.erc20 != address(0) && !_chainDetails.isNativeGasToken) {
-        _lockbox = factory.deployLockbox(_xerc20, _chainDetails.erc20, _chainDetails.isNativeGasToken);
-      }
-
-      // transfer xerc20 ownership to the governor
-      XERC20(_xerc20).transferOwnership(_chainDetails.governor);
+      // transfer xerc721 ownership to the governor
+      XERC721(_xerc721).transferOwnership(_chainDetails.governor);
 
       vm.stopBroadcast();
 
       // solhint-disable-next-line no-console
       console.log('Deployment to chain with RPC name: ', _chainDetails.rpcEnvName);
       // solhint-disable-next-line no-console
-      console.log('xERC20 token deployed: ', _xerc20);
-      if (_lockbox != address(0)) {
-        // solhint-disable-next-line no-console
-        console.log('Lockbox deployed: ', _lockbox);
-      }
-      _tokens[i] = _xerc20;
+      console.log('xERC721 token deployed: ', _xerc721);
+      _tokens[i] = _xerc721;
     }
 
     if (_chainAmount > 1) {
--- solidity/scripts/XERC721FactoryDeploy.sol
+++ solidity/scripts/XERC721FactoryDeploy.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.4 <0.9.0;
 
 // solhint-disable-next-line no-console
 import {console} from 'forge-std/console.sol';
-import {XERC20Factory, IXERC20Factory} from '../contracts/XERC20Factory.sol';
+import {XERC721Factory, IXERC721Factory} from '../contracts/XERC721Factory.sol';
 import {Script} from 'forge-std/Script.sol';
 import {stdJson} from 'forge-std/StdJson.sol';
 import {ScriptingLibrary} from './ScriptingLibrary/ScriptingLibrary.sol';
@@ -19,30 +19,30 @@ struct BridgeDetails {
 
 struct ChainDetails {
   BridgeDetails[] bridgeDetails; // The array of bridges to configure for this chain
-  address erc721; // The address of the ERC20 canonical token of that chain (address(0) if none)
-  address governor; // The governor address of the xERC20
-  bool isNativeGasToken; // Whether or not the token is the native gas token of the chain. E.g. Are you deploying an xERC20 for MATIC in Polygon?
+  address erc721; // The address of the ERC721 canonical token of that chain (address(0) if none)
+  address governor; // The governor address of the xERC721
+  bool isNativeGasToken; // Whether or not the token is the native gas token of the chain. E.g. Are you deploying an xERC721 for MATIC in Polygon?
   string rpcEnvName; // The name of the RPC to use from the .env file
 }
 
 struct DeploymentConfig {
   ChainDetails[] chainDetails;
-  string name; // The name to use for the xERC20
-  string symbol; // The symbol to use for the xERC20
+  string name; // The name to use for the xERC721
+  string symbol; // The symbol to use for the xERC721
 }
 
-contract XERC20FactoryDeploy is Script, ScriptingLibrary {
+contract XERC721FactoryDeploy is Script, ScriptingLibrary {
   using stdJson for string;
 
   ////////////////////////// MODIFY ////////////////////////////////
   // When new factories need to be deployed, make sure to update the salt version to avoid address collition
-  string public constant SALT = 'xERC20-v1.6';
+  string public constant SALT = 'xERC721-v1.5';
   //////////////////////////////////////////////////////////////////
 
   uint256 public deployerPk = vm.envUint('DEPLOYER_PRIVATE_KEY');
 
   function run() public {
-    string memory _json = vm.readFile('./solidity/scripts/xerc20-deployment-config.json');
+    string memory _json = vm.readFile('./solidity/scripts/xerc721-deployment-config.json');
     DeploymentConfig memory _data = abi.decode(_json.parseRaw('.'), (DeploymentConfig));
     uint256 _chainAmount = _data.chainDetails.length;
 
@@ -55,11 +55,11 @@ contract XERC20FactoryDeploy is Script, ScriptingLibrary {
 
       vm.startBroadcast(deployerPk);
 
-      XERC20Factory _factory = new XERC20Factory{salt: _salt}();
+      XERC721Factory _factory = new XERC721Factory{salt: _salt}();
       vm.stopBroadcast();
 
       // solhint-disable-next-line no-console
-      console.log('Factory XERC20 deployed to:', address(_factory));
+      console.log('Factory XERC721 deployed to:', address(_factory));
     }
   }
 }
