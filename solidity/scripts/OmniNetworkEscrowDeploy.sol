bc01fc17cc7d3f01ede831e8a7c2546242197d5d
Paterson <86971279+paterson1@users.noreply.github.com>
2024-03-05 22:13:38 -0300

    new deploy

--- solidity/scripts/OmniNetworkEscrowDeploy.sol
+++ solidity/scripts/OmniNetworkEscrowDeploy.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.4 <0.9.0;
 
+// solhint-disable-next-line no-console
 import {console} from 'forge-std/console.sol';
 import {OmniNetworkEscrow} from '../contracts/OmniNetworkEscrow.sol';
 import {Script} from 'forge-std/Script.sol';
@@ -8,13 +9,13 @@ import {ScriptingLibrary} from './ScriptingLibrary/ScriptingLibrary.sol';
 
 contract OmniNetworkEscrowDeploy is Script, ScriptingLibrary {
   uint256 public deployer = vm.envUint('DEPLOYER_PRIVATE_KEY');
-  address public deployerAddress = vm.envAddress('DEPLOYER_ADDRESS');
+  address public relayerAddress = vm.envAddress('RELAYER_ADDRESS');
 
   function run() public {
     vm.createSelectFork(vm.rpcUrl(vm.envString('ETHEREUM_SEPOLIA_RPC')));
 
     vm.startBroadcast(deployer);
-    OmniNetworkEscrow _escrow = new OmniNetworkEscrow(deployerAddress);
+    OmniNetworkEscrow _escrow = new OmniNetworkEscrow(relayerAddress);
     vm.stopBroadcast();
 
     // solhint-disable-next-line no-console
